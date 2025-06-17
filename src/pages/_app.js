62b40d2617c2498a2085c2489bb24254c4f18c04
ckx9798 <ckx12@naver.com>
2025-06-17 16:31:37 +0900

    Feat: App컴포넌트 - 뒷 배경 구현

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,40 @@
 import "@/styles/globals.css";
+import "highlight.js/styles/github-dark.css";
+
+import EarthGlobe from "@/components/Animation/EarthGlobe";
+import Navbar from "@/components/Navbar";
+import ShootingStars from "@/components/Animation/ShootingStars";
+import SubNav from "@/components/SubNav";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const isHome = router.pathname === "/";
+
+  return (
+    <div className="relative">
+      {/* 별똥별 배경 (최하단) */}
+      <ShootingStars />
+
+      {/* 콘텐츠 레이어 */}
+      <div className="relative z-10">
+        <Navbar />
+        <div className="flex justify-center md:px-10 lg:gap-40 lg:px-0">
+          {!isHome && (
+            <div className="hidden px-4 md:block lg:px-0">
+              <SubNav />
+            </div>
+          )}
+          <main
+            className={`w-full px-4 md:px-10 ${
+              isHome ? "lg:w-[1700px]" : "py-14 lg:w-[900px]"
+            }`}
+          >
+            <Component {...pageProps} />
+            <EarthGlobe />
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 }
