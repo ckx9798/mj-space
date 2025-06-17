963c8c112d6ae4c3593361e39ec24f9e2c25c992
ckx9798 <ckx12@naver.com>
2025-06-17 12:57:00 +0900

    Feat: 메인 컴포넌트 텍스트 추가 및 블로그 오픈 구현

--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { ChevronRight, Milestone } from "lucide-react";
 
 import RecentPosts from "./RecentPost";
@@ -33,14 +34,16 @@ const IntroSection = () => {
           </span>
         </h1>
 
-        <span>"새로움에 도전하는 프론트엔드 개발자."</span>
+        <span className="text-md md:text-xl">
+          "할 줄 아는 것보다, 할 수 있게 되는 과정을 즐기는 개발자"
+        </span>
 
-        <p className="mb-10 text-sm leading-loose text-[#b0b3c6] md:text-xl">
+        <p className="mb-10 text-sm leading-loose text-[#b0b3c6] md:text-lg">
           <br />
-          직관적이고 아름다운 UI/UX를 구현하며, 최신 웹 트렌드와 기술을 빠르게
-          습득합니다.
+          작은 불편을 기술로 해결하고, 더 나은 제품을 만들어가는 과정에 관심이
+          많습니다.
           <br />
-          기술과 소통의 균형을 통해 차별화된 경험을 만듭니다.
+          ‘처음이라서 못 한다’는 말보단, ‘필요하니 해본다’는 태도를 지향합니다.
         </p>
       </div>
     </div>
@@ -71,11 +74,19 @@ const PostToggleButton = () => {
             />
           </button>
 
-          {isOpen && (
-            <div className="absolute left-0 top-full z-10 mt-8 w-full">
-              <RecentPosts />
-            </div>
-          )}
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3 }}
+                className="absolute left-0 top-full z-10 mt-8 w-full"
+              >
+                <RecentPosts />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </div>
